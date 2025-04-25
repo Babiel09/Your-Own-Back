263ae900ba3cd2f4e156707d13dcd9682b605054
Gabriel Castro <estudosbabiel@gmail.com>
2025-04-24 21:29:02 -0300

    config: Importing Prisma Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { RedisModule } from 'src/redis/redis.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [RedisModule],
+  imports: [RedisModule,PrismaModule],
 })
 export class UserModule {}
