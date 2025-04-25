4b05aacc38503b0da4a2afaf7bad30ec4d611ccd
Gabriel Castro <estudosbabiel@gmail.com>
2025-04-24 21:07:07 -0300

    config: Configured the redis service

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService extends Redis{
+    private logger: Logger = new Logger(RedisService.name)
+    constructor(){ 
+        super();
+        if(super.on){   
+            this.logger.log("Redis working!");
+        }else{
+            this.logger.error("Redis server error!");
+        };
+    
+    }
+};
