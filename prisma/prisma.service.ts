815c6a800d1aba186ca2711e19defcbb1d91aa91
Gabriel Castro <estudosbabiel@gmail.com>
2025-04-24 21:28:29 -0300

    feat: Made the Prisma Service a daughter of the PrismaClient

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'generated/prisma';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super();
+    };
+};
