628fac91b34c1b75e0b8b17d82ff54c3c275aca1
Gabriel Castro <estudosbabiel@gmail.com>
2025-04-24 21:09:28 -0300

    config: Importing RedisModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  imports: [RedisModule],
 })
 export class UserModule {}
