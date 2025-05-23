f68ccbf8938f062889313525a782a2f8156acf55
Gabriel Castro <estudosbabiel@gmail.com>
2025-05-23 12:50:41 -0300

    feat: Created new DTO to create the users

---
+++ src/user/DTO/user.create.dto.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { IsEmail, IsInt, IsString, MaxLength, MinLength } from "class-validator";
+
+@Injectable()
+export class CreateUser{
+    
+    @IsString({message:"The name must be a string!"})
+    name:string;
+
+    @IsEmail(undefined,{message:"The email needs to follow this standart: youremail@yourprovider.com"})
+    email:string;
+
+    @IsString({message:"The password must be a string!"})
+    @MinLength(12,{message:"The password needs to have 8 caractheres to be accept"})
+    password:string;
+
+    @IsString({message:"The bio must be a string!"})
+    @MaxLength(12,{message:"The bio only can have 150 caractheres"})
+    bio:string;
+};
