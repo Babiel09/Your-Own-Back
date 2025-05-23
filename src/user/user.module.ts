da0bd45aa30abbfe672be414a56629eb06c74147
Gabriel Castro <estudosbabiel@gmail.com>
2025-05-23 13:45:20 -0300

    fixed: Made the CreateUserDTo a provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { RedisModule } from 'src/redis/redis.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { CreateUserDTO } from './DTO/user.create.dto';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService,CreateUserDTO],
   imports: [RedisModule,PrismaModule],
 })
 export class UserModule {}
