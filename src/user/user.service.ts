a5fac0dda9539e3cc273400ac680d161271b736e
Gabriel Castro <estudosbabiel@gmail.com>
2025-05-23 12:21:55 -0300

    fixed: Fixed prisma version to the 6.0.1

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RedisService } from 'src/redis/redis.service';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private logger:Logger = new Logger(UserService.name);
+    constructor(private redisservice:RedisService, private prismaService:PrismaService){};
+};
